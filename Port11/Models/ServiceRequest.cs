d819805de050a4db966a924cacf380b1d982a87c
Gregory Gismondi <gismondigm@upmc.edu>
2019-02-04 20:55:27 -0500

    added unit tests

--- Port11/Models/ServiceRequest.cs
+++ Port11/Models/ServiceRequest.cs
@@ -1,5 +1,4 @@
 ﻿using System.Collections.Generic;
-using Port11.Utilities;
 using RestSharp;
 
 namespace Port11.Models
@@ -11,7 +10,7 @@ namespace Port11.Models
         public string Json { get; set; }
         public Method Method { get; set; }
         public string TestName { get; set; }
-        public List<ResponseHeader> ResponseHeaders = new List<ResponseHeader>();
+        public List<ResponseHeader> ResponseHeaders = new List<ResponseHeader> {new ResponseHeader{Name = "Content-Type" , Value = "application/json" } };
         public bool ReturnAsJson { get; set; }
     }
 }
