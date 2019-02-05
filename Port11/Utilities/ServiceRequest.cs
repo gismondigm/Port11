e29e555082f1d5f13248dcfa7af98113c3d21c7c
Gregory Gismondi <gismondigm@upmc.edu>
2019-02-04 20:02:06 -0500

    initial add

---
+++ Port11/Utilities/ServiceRequest.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using RestSharp;
+namespace Port11
+{
+    public class ServiceRequest
+    {
+        public string Service { get; set; }
+        public string Url { get; set; }
+        public string Json { get; set; }
+        public Method Method { get; set; }
+        public string TestName { get; set; }
+    }
+}
